d9b000166f734607c3bb5cb0e8a797df358007ff
yoshimaputri <yoshimashima@gmail.com>
2017-10-23 04:57:46 +0000

    no2 modified | but segmentation fault (core dumped)

--- no2.c
+++ no2.c
@@ -4,16 +4,36 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-pthread_t tid[2];
+pthread_t tid[100]; int N,T,group;
 
-void* prime(void *arg)
+void* prime(int N,int T)
 {
 	pthread_t id = pthread_self();
+	int a;
+	while(a<T){
+	group = N/T;
 	if(pthread_equal(id,tid[0]))
-	{
+	{	
+		for(int x=2; x<=group; x++){
+			int cek=1;
+			for(int j=3;j<=group;j++){
+				if(x%j==0) cek=0;
+			}
+			if(cek==1) printf("%d ", x);
+		}
 	}
-	else if(pthread_equal(id,tid[1]))
+	else if(pthread_equal(id,tid[!0]))
 	{
+		int awal=(group*a)+1;
+		int akhir=group*(a+1);
+		for(int x=awal;x<=akhir;x++){
+			int cek=1;
+			for(int j=awal+1;j<=akhir;j++){
+				if(x%j==0) cek=0;
+			}
+			if(cek==1) printf("%d ", x);
+		}
+	}
 	}
 	return NULL;
 }
@@ -21,14 +41,19 @@ void* prime(void *arg)
 int main()
 {
 	int i; int err;
-	while(i<2)
+	printf("Masukkan N batas bilangan dan T jumlah thread : ");
+	scanf("%d %d", &N, &T);
+	while(i<T)
 	{
-		err=pthread_create(&(tid[i]),NULL,&prime,NULL);
+		err=pthread_create(&(tid[i]),NULL,prime(N,T),NULL);
 		if(err!=0) printf("can't create thread : [%s]",strerror(err));
 		else printf("create thread success");
 		i++;
 	}
-	pthread_join(tid[0],NULL);
-	pthread_join(tid[1],NULL);
+	i=0;
+	while(i<T){
+		pthread_join(tid[i],NULL);
+		i++;
+	}
 	return 0;
 }
